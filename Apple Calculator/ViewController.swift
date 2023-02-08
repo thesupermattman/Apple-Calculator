84acfd4d6cc0c8a0f67d7707ccea4b75a57928ee
Arthithai Aamlid <osacn_arthithai.aamlid@krungthai.com>
2023-02-08 11:57:24 +0700

    Initial Commit

---
+++ Apple Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Calculator
+//
+//  Created by arthithai.aamlid on 8/2/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
