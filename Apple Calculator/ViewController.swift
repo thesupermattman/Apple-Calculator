1edccbc9e461841bb9a5954c1425e5387da2b5ef
Arthithai Aamlid <osacn_arthithai.aamlid@krungthai.com>
2023-02-10 16:45:33 +0700

    Everything works well but not the percent and positive negative button

--- Apple Calculator/ViewController.swift
+++ Apple Calculator/ViewController.swift
@@ -8,68 +8,91 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var viewModel = ViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBOutlet weak var CalculatorLabel: UILabel!
+    @IBOutlet weak var calculatorLabel: UILabel!
     
     @IBAction func buttonAC(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.reset()
     }
     
     @IBAction func buttonPositiveNegative(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "+/-")
     }
     
     @IBAction func buttonPercent(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "%")
+
     }
     
     @IBAction func buttonDivide(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "/")
     }
     
     @IBAction func buttonMultiply(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "*")
     }
     
     @IBAction func buttonMinus(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "-")
     }
     
     @IBAction func buttonAdd(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "+")
     }
     
     @IBAction func buttonEquals(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.equals()
     }
     
     @IBAction func button0(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "0")
     }
     
     @IBAction func button1(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "1")
     }
     
     @IBAction func button2(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "2")
     }
     
     @IBAction func button3(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "3")
     }
     
     @IBAction func button4(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "4")
     }
     
     @IBAction func button5(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "5")
     }
     
     @IBAction func button6(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "6")
     }
     
     @IBAction func button7(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "7")
     }
     
     @IBAction func button8(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "8")
     }
     
     @IBAction func button9(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: "9")
     }
     
     @IBAction func buttonDecimal(_ sender: UIButton) {
+        calculatorLabel.text = viewModel.calculate(value: ".")
+
     }
 }
--- Apple Calculator/ViewModel.swift
+++ Apple Calculator/ViewModel.swift
@@ -9,4 +9,109 @@ import Foundation
 
 class ViewModel {
     
+    var latestNumber = ""
+    var latestResultNumber: Float? = nil
+    var latestOperator = ""
+    var isEqualTapped = false
+    let plusSign = "+"
+    
+    func calculate(value: String) -> String {
+        if value == plusSign {
+//            if isEqualTapped {
+//                latestOperator = value
+//                //latestResultNumber = (Float(latestNumber) ?? 0) + (latestResultNumber ?? 0)
+//                latestNumber = ""
+//                isEqualTapped = false
+//                return String((latestResultNumber ?? 0))
+//            } else {
+//                latestOperator = value
+//                latestResultNumber = (Float(latestNumber) ?? 0) + (latestResultNumber ?? 0)
+//                latestNumber = ""
+//                isEqualTapped = false
+//                return String((latestResultNumber ?? 0))
+//            }
+            latestOperator = value
+            latestResultNumber = isEqualTapped ? latestResultNumber : (Float(latestNumber) ?? 0) + (latestResultNumber ?? 0)
+            latestNumber = ""
+            isEqualTapped = false
+            return String((latestResultNumber ?? 0))
+        } else if value == "-" {
+            latestOperator = value
+            if let notNilValue = latestResultNumber {
+                latestResultNumber = isEqualTapped ? latestResultNumber : notNilValue - (Float(latestNumber) ?? 0)
+            } else {
+                latestResultNumber = isEqualTapped ? latestResultNumber : Float(latestNumber)
+            }
+            latestNumber = ""
+            isEqualTapped = false
+            return String((latestResultNumber ?? 0))
+        } else if value == "*" {
+            latestOperator = value
+            if let notNilValue = latestResultNumber {
+                latestResultNumber = isEqualTapped ? latestResultNumber : notNilValue * (Float(latestNumber) ?? 0)
+            } else {
+                latestResultNumber = isEqualTapped ? latestResultNumber : Float(latestNumber)
+            }
+            latestNumber = ""
+            return String((latestResultNumber ?? 0))
+        } else if value == "/" {
+            latestOperator = value
+            if let notNilValue = latestResultNumber {
+                latestResultNumber = isEqualTapped ? latestResultNumber : notNilValue / (Float(latestNumber) ?? 0)
+            } else {
+                latestResultNumber = isEqualTapped ? latestResultNumber : Float(latestNumber)
+            }
+            latestNumber = ""
+            isEqualTapped = false
+            return String(latestResultNumber ?? 0)
+        } else if value == "%"{
+            latestOperator = value
+            latestResultNumber = (Float(latestNumber) ?? 0) / 100
+            latestNumber = String((latestResultNumber ?? 0))
+            latestResultNumber = 0
+            isEqualTapped = false
+            return latestNumber
+        } else if value == "+/-" {
+            latestOperator = value
+            latestResultNumber = (Float(latestNumber) ?? 0) * -1
+            latestNumber = String((latestResultNumber ?? 0))
+            return latestNumber
+        } else { // MARK: Only number
+            // latestNumber += value
+            latestNumber = latestNumber + value
+            return latestNumber
+        }
+    }
+    
+    func reset() -> String {
+        latestNumber = ""
+        latestResultNumber = nil
+        latestOperator = ""
+        return "0.0"
+    }
+    
+    func equals() -> String {
+        isEqualTapped = true
+        if latestOperator == "+" {
+            latestResultNumber = (Float(latestNumber) ?? 0) + (latestResultNumber ?? 0)
+            return String((latestResultNumber ?? 0))
+        } else if latestOperator == "-" {
+            latestResultNumber = (latestResultNumber ?? 0) - (Float(latestNumber) ?? 0)
+            return String((latestResultNumber ?? 0))
+        } else if latestOperator == "*" {
+            latestResultNumber = (Float(latestNumber) ?? 0) * (latestResultNumber ?? 0)
+            return String((latestResultNumber ?? 0))
+        } else if latestOperator == "/" {
+            latestResultNumber = (latestResultNumber ?? 0) / (Float(latestNumber) ?? 0)
+            return String((latestResultNumber ?? 0))
+        } else {
+            return "0"
+        }
+    }
 }
+
+// To do
+// Fix percent button
+// Fix positive negative
+// Make buttons round
+// If the above is done, then try to do the model. Also see what I could make an enum.
